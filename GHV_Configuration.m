091fcce965fadd74f19014c1c1c04bdff6db1a33
blueWALL-E <boyu.dragon@gmail.com>
2025-05-24 23:58:19 +0800

    feat: 更新GHV_Configuration.m文件
    飞行器基本参数改写为结构体形式方便调用；所有初始化统一写成变量，方便后续调用和修改

--- GHV_Configuration.m
+++ GHV_Configuration.m
@@ -2,7 +2,7 @@
 /*
 * @Author:blueWALL - E
 * @Date:2025 - 05 - 23 21:43:50
- * @LastEditTime: 2025-05-23 22:47:38
+ * @LastEditTime: 2025-05-24 22:21:02
  * @FilePath: \GHV_open\GHV_Configuration.m
 * @Description: 飞行器基本参数信息
 * @Wearing:Read only, do not modify place ! !!
@@ -10,7 +10,7 @@
 */
 %}
 
-%常用的单位换算常数
+%单位换算常数 尽量避免使用吧 但保留这个接口
 d2r = pi / 180; % Conversion Deg to Rad
 g = 9.81; % Gravity [m/s/s]
 m2ft = 3.28084; % meter to feet
@@ -19,16 +19,20 @@ Kg2lb = 2.20462; % Kg to lb
 
 % 飞行器基本参数
 % 需要使用的飞行器自身的参数-标准单位制 米 牛顿
+GHV_cfg = struct();
+GHV_cfg.c_ref = 24.38; %单位 m 机翼平均弦长
+GHV_cfg.b_ref = 18.29; %单位 m 机翼展长
+GHV_cfg.s_ref = 334.73; %机翼参考面积 单位 m2
+GHV_cfg.x_cg = 2.9; %单位 m 力矩中心到质心的距离
+GHV_cfg.mass_full = 136077; %单位 kg 飞行器最大质量
+GHV_cfg.Jx = 1.36 * 10 ^ 6; %单位 kg*m^2 机体x轴的转动惯量
+GHV_cfg.Jy = 1.5 * 10 ^ 7; %单位 kg*m^2 机体x轴的转动惯量
+GHV_cfg.Jz = 1.36 * 10 ^ 7; %单位 kg*m^2 机体x轴的转动惯量
+GHV_cfg.J = diag([GHV_cfg.Jx, GHV_cfg.Jy, GHV_cfg.Jz]); %单位 kg*m^2 机体转动惯量矩阵
+Simulink.Bus.createObject(GHV_cfg); %名字是slBus1
 
-b_ref = 18.29; %机翼翼展 单位 m
-c_ref = 24.38; %平均几何弦长 单位 m
-s_ref = 334.73; %机翼参考面积 单位 m2
-x_cg = 2.9; %单位 m 力矩中心到质心的距离
-mass_full = 136077; %单位 kg 飞行器最大质量
-Jx = 1.36 * 10 ^ 6; %单位 kg*m^2 机体x轴的转动惯量
-Jy = 1.5 * 10 ^ 7; %单位 kg*m^2 机体x轴的转动惯量
-Jz = 1.36 * 10 ^ 7; %单位 kg*m^2 机体x轴的转动惯量
-
-%飞行器初始位置
-xme_0 = [0; 0; -10000]; % Initial Position
-Aimpoint = 0;
+%飞行器初始状态
+Position_init = [0; 0; -50000]; % 初始位置 大地坐标系
+Euler_init = [0; 0; 0]; %初始姿态 欧拉角 机体坐标系
+Omega_init = [0; 0; 0]; %初始角速度 机体坐标系
+Speed_init = [3000; 0; 0]; %初始速度 机体坐标系
