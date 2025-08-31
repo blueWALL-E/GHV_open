92e05ee7cbe6792dc99c4d6c45691a28050beaa0
blueWALL-E <boyu.dragon@gmail.com>
2025-08-31 21:47:40 +0800

    chore: GHV_Configuration 更新时间自动更新 无实质内容改动

--- GHV_Configuration.m
+++ GHV_Configuration.m
@@ -2,7 +2,7 @@
 /*
 * @Author:blueWALL-E
 * @Date:2025-05-23 21:43:50
- * @LastEditTime: 2025-07-08 11:57:10
+ * @LastEditTime: 2025-08-30 20:59:04
  * @FilePath: \GHV_open\GHV_Configuration.m
 * @Description: 飞行器基本参数信息
 * @Wearing:Read only, do not modify place !!!
