bf9e23e13912f768017831d513a7a61d55f50d65
blueWALL-E <boyu.dragon@gmail.com>
2025-09-21 23:41:22 +0800

    feat: 更新组合发动机推力模型，添加推力矢量分解 力矩分解尚未解决

--- GHV_model/Propulsion_model.m
+++ GHV_model/Propulsion_model.m
@@ -2,7 +2,7 @@
 /*
 * @Author:blueWALL-E
 * @Date:2025-09-15 14:33:57
- * @LastEditTime: 2025-09-21 22:42:11
+ * @LastEditTime: 2025-09-21 23:30:22
  * @FilePath: \GHV_open\GHV_model\Propulsion_model.m
 * @Description: 组合发动机推力模型
 * @Wearing:Read only, do not modify place !!!
@@ -15,23 +15,26 @@
 % PLA    单位 [%] 油门开度
 % H     单位 m 飞行高度
 % Ma    单位 n.d. 马赫数
+% delta_y 单位 deg 矢量发动机偏转角
+% delta_z 单位 deg 矢量发动机偏转角
 
 %output
 % T     单位 N 推力
 % Isp   单位 s 比冲
 
-function [T, Isp] = Propulsion_model(PLA, H, Ma)
-
+function [T, Isp] = Propulsion_model(PLA, H, Ma, delta_y, delta_z)
+    %输出矩阵定义
+    T = zeros(3, 1); %#ok<PREALL>
     %输入检查
     if Ma < 0 || Ma > 24
         error(' Mach number input error, should be between 0 and 24');
     end
 
     if Ma <= 2 %涡轮发动机
-        T = PLA * (2.99e-8 -32.81 * H +1.43e-3 * H ^ 2 -2.59e-8 * H ^ 3 +3.75e3 * Ma);
+        T_norm = PLA * (2.99e-8 -32.81 * H +1.43e-3 * H ^ 2 -2.59e-8 * H ^ 3 +3.75e3 * Ma);
 
     elseif Ma <= 6 %冲压发动机
-        T = PLA * (3.93e-8 ...
+        T_norm = PLA * (3.93e-8 ...
             +3.94e5 * Ma ...
             -6.97e5 * Ma .^ 2 ...
             +8.07e5 * Ma .^ 3 ...
@@ -43,12 +46,18 @@ function [T, Isp] = Propulsion_model(PLA, H, Ma)
     else %火箭发动机
 
         if H < 17373.6
-            T = -5.43e4 + 2.178 * H +3.24e5 * PLA + 0.374 * H * PLA;
+            T_norm = -5.43e4 + 2.178 * H +3.24e5 * PLA + 0.374 * H * PLA;
         else
-            T = -1.64e4 +6.69295e5 * PLA;
+            T_norm = -1.64e4 +6.69295e5 * PLA;
         end
 
     end
+
+    %推力矢量分解 //TODO 力矩的计算还没有解决
+    Tx = T_norm * cosd(delta_y) * cosd(delta_z);
+    Ty = T_norm * cosd(delta_z) * sind(delta_y);
+    Tz = T_norm * sind(delta_z);
+    T = [Tx; Ty; Tz];
 
     %比冲模型
     Isp = 1867 ...
