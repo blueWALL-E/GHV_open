0e15ac2b6485ad82d2647fbf846311c7a75c8a6a
blueWALL-E <boyu.dragon@gmail.com>
2025-09-21 22:44:57 +0800

    feat: 更新组合发动机推力模型，添加比冲计算公式

--- GHV_model/Propulsion_model.m
+++ GHV_model/Propulsion_model.m
@@ -2,7 +2,7 @@
 /*
 * @Author:blueWALL-E
 * @Date:2025-09-15 14:33:57
- * @LastEditTime: 2025-09-20 22:35:24
+ * @LastEditTime: 2025-09-21 22:42:11
  * @FilePath: \GHV_open\GHV_model\Propulsion_model.m
 * @Description: 组合发动机推力模型
 * @Wearing:Read only, do not modify place !!!
@@ -18,17 +18,19 @@
 
 %output
 % T     单位 N 推力
+% Isp   单位 s 比冲
 
-function T = Propulsion_model(PLA, H, Ma)
+function [T, Isp] = Propulsion_model(PLA, H, Ma)
 
+    %输入检查
     if Ma < 0 || Ma > 24
         error(' Mach number input error, should be between 0 and 24');
     end
 
-    if Ma <= 2 % (1)涡轮发动机
+    if Ma <= 2 %涡轮发动机
         T = PLA * (2.99e-8 -32.81 * H +1.43e-3 * H ^ 2 -2.59e-8 * H ^ 3 +3.75e3 * Ma);
 
-    elseif Ma <= 6 % (2) 冲压发动机
+    elseif Ma <= 6 %冲压发动机
         T = PLA * (3.93e-8 ...
             +3.94e5 * Ma ...
             -6.97e5 * Ma .^ 2 ...
@@ -38,7 +40,7 @@ function T = Propulsion_model(PLA, H, Ma)
             -1.50e4 * Ma .^ 6 ...
             +7.53e2 * Ma .^ 7);
 
-    else % (3) 火箭发动机
+    else %火箭发动机
 
         if H < 17373.6
             T = -5.43e4 + 2.178 * H +3.24e5 * PLA + 0.374 * H * PLA;
@@ -47,5 +49,14 @@ function T = Propulsion_model(PLA, H, Ma)
         end
 
     end
+
+    %比冲模型
+    Isp = 1867 ...
+        + 1454 * Ma ...
+        - 406.2 * Ma .^ 2 ...
+        + 44.49 * Ma .^ 3 ...
+        - 2.453 * Ma .^ 4 ...
+        + 0.06806 * Ma .^ 5 ...
+        - 0.0007576 * Ma .^ 6;
 
 end
