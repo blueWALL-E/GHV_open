c74749d7db4fb6e667fb907a9680dbfaa2324c6d
blueWALL-E <boyu.dragon@gmail.com>
2025-09-23 00:53:43 +0800

    refactor: 将重心位置改成向量形式 统一接口 方便后续调用

--- GHV_model/cg_inertia_variation.m
+++ GHV_model/cg_inertia_variation.m
@@ -2,8 +2,8 @@
 /*
  * @Author: blueWALL-E
  * @Date: 2025-09-22 10:10:22
- * @LastEditTime: 2025-09-22 11:17:18
- * @FilePath: \GHV_open\GHV_model\mass_properties.m
+ * @LastEditTime: 2025-09-22 23:59:27
+ * @FilePath: \GHV_open\GHV_model\cg_inertia_variation.m
  * @Description: 质心位置变化与转动惯量变化计算
  * @Wearing:  Read only, do not modify place!!!
  * @Shortcut keys:  ctrl+alt+/ ctrl+alt+z
@@ -27,7 +27,8 @@ function [x_cg, I, dI_dmass] = cg_inertia_variation(mass)
     I = diag([Ixx, Iyy, Izz]);
 
     % 重心位置
-    x_cg = 1.65e-10 * mass ^ 2 -5.57e-5 * mass + 7.37;
+    x_cg_element = 1.65e-10 * mass ^ 2 -5.57e-5 * mass + 7.37;
+    x_cg = [-x_cg_element, 0, 0];
 
     % 转动惯量对质量的导数
     dIxx_dmass = 1991/100 - (5238875316933513 * mass) / 36893488147419103232;
