9826000271512f7ce23b1e5d6627f5193b1bce55
blueWALL-E <boyu.dragon@gmail.com>
2025-10-11 23:24:36 +0800

    chore(GHV_config): 仿真测试中会影响最后更新时间 无实质内容更新

--- GHV_Configuration.m
+++ GHV_Configuration.m
@@ -2,7 +2,7 @@
 /*
  * @Author:blueWALL-E
  * @Date:2025-05-23 21:43:50
- * @LastEditTime: 2025-10-08 11:15:34
+ * @LastEditTime: 2025-10-11 22:14:53
  * @FilePath: \GHV_open\GHV_Configuration.m
  * @Description: 飞行器基本参数信息
  * @Wearing:Read only, do not modify place !!!
