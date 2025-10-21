7b1aae45c3b0eb5f3b8d47514d0c49b5000e725b
blueWALL-E <boyu.dragon@gmail.com>
2025-10-21 14:14:20 +0800

    refactor(traj): 改升力阻力发动机推力为是带方向的，并同步修改公式正负号
    推力 阻力 升力改为在机体坐标系下的实际值，并非绝对值。在这种情况下，阻力和升力均为负数，推力为整数。

--- GHV_trajectory/three_dof_trajectory.m
+++ GHV_trajectory/three_dof_trajectory.m
@@ -2,7 +2,7 @@
 /*
  * @Author: blueWALL-E
  * @Date: 2025-10-21 10:18:50
- * @LastEditTime: 2025-10-21 10:27:25
+ * @LastEditTime: 2025-10-21 11:05:37
  * @FilePath: \GHV_open\GHV_trajectory\three_dof_trajectory.m
  * @Description: 垂直平面内三自由度轨迹方程
  * @Wearing:  Read only, do not modify place!!!
@@ -12,8 +12,8 @@
 %垂直平面内三自由度轨迹方程
 %input:
 %T:      单位 N       推力
-%D:      单位 N       阻力
-%L:      单位 N       升力
+%D:      单位 N       阻力（在机体坐标系下）
+%L:      单位 N       升力（在机体坐标系下）
 %alpha:  单位 rad     攻角
 %m:      单位 kg      质量
 %g:      单位 m/s^2   重力加速度
@@ -27,8 +27,8 @@
 %d_h:      单位 m/s     垂直位置变化率
 function [d_V, d_gamma, d_x, d_h] = three_dof_trajectory(T, D, L, alpha, m, g, V, gamma)
 
-    d_V = (T * cos(alpha) - D) / m - g * sin(gamma);
-    d_gamma = ((T * sin(alpha) + L) / m - g * cos(gamma)) / V;
+    d_V = (T * cos(alpha) + D) / m - g * sin(gamma);
+    d_gamma = ((T * sin(alpha) - L) / m - g * cos(gamma)) / V;
     d_x = V * cos(gamma);
     d_h = V * sin(gamma);
 
