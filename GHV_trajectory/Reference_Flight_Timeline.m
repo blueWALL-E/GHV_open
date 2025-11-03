c0f1deef95a369d8ef44c4c558378ff8c6d8f638
blueWALL-E <boyu.dragon@gmail.com>
2025-11-03 19:47:05 +0800

    refactor(traj): 更新一个新的飞行时序
    新飞行时序全程在大气层内飞行，可仅用舵面完成姿态控制

--- GHV_trajectory/Reference_Flight_Timeline.m
+++ GHV_trajectory/Reference_Flight_Timeline.m
@@ -2,7 +2,7 @@
 /*
  * @Author: blueWALL-E
  * @Date: 2025-10-19 14:52:06
- * @LastEditTime: 2025-11-01 22:41:21
+ * @LastEditTime: 2025-11-03 13:47:06
  * @FilePath: \GHV_open\GHV_trajectory\Reference_Flight_Timeline.m
  * @Description: 参考飞行时序
  * @Wearing:  Read only, do not modify place!!!
@@ -29,14 +29,39 @@ function [aero_ang, Control_Propulsion] = Reference_Flight_Timeline(t, Ma)
     p5 = -9.176;
     p6 = 25.62;
 
-    if t <= 140 %时间小于130s 时 维持高攻角爬升
+    % if t <= 140 %时间小于130s 时 维持高攻角爬升
+    %     alpha = 6;
+    %     PLA = 1;
+    % elseif t <= 2500 % 时间在130s到2500s之间 时 采用最佳升阻比攻角 跳跃滑翔
+    %     alpha = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
+    %     PLA = 0;
+    % else % 时间大于1873s 时 俯冲
+    %     alpha = 0.1;
+    %     PLA = 0;
+    % end
+
+    % if t <= 60
+    %     alpha = 6;
+    %     PLA = 1;
+    % else
+    %     alpha = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
+    %     PLA = 0;
+    % end
+
+    transition_time = 2; % 过渡时间（秒）
+    t1 = 60; % 起始时间点 130
+
+    if t < t1
         alpha = 6;
         PLA = 1;
-    elseif t <= 2500 % 时间在130s到2500s之间 时 采用最佳升阻比攻角 跳跃滑翔
-        alpha = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
+    elseif t < t1 + transition_time
+        % 平滑过渡：从 alpha = 6 到多项式计算值
+        alpha_start = 6;
+        alpha_end = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
+        alpha = alpha_start + (alpha_end - alpha_start) * (t - t1) / transition_time;
         PLA = 0;
-    else % 时间大于1873s 时 俯冲
-        alpha = 0.1;
+    else
+        alpha = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
         PLA = 0;
     end
 
