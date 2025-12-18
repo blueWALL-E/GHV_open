5eeff49e6cd6d31d0b75baf7cf6d284b6b596c98
blueWALL-E <boyu.dragon@gmail.com>
2025-12-18 11:19:53 +0800

    fix(traj): 将飞行时序调整为标准飞行时序
    前60s按6°攻角飞行 60-840按最佳升阻比攻角飞行

--- GHV_trajectory/Reference_Flight_Timeline.m
+++ GHV_trajectory/Reference_Flight_Timeline.m
@@ -2,7 +2,7 @@
 /*
  * @Author: blueWALL-E
  * @Date: 2025-10-19 14:52:06
- * @LastEditTime: 2025-12-10 16:04:52
+ * @LastEditTime: 2025-12-18 11:09:11
  * @FilePath: \GHV_open\GHV_trajectory\Reference_Flight_Timeline.m
  * @Description: 参考飞行时序
  * @Wearing:  Read only, do not modify place!!!
@@ -21,7 +21,6 @@
 %Control_Propulsion:  单位 n.d. rad/s  发动机油门量与矢量控制角度 3*1
 function [d_aero_ang, Control_Propulsion] = Reference_Flight_Timeline(t, Ma)
 
-    % 攻角变化规律
     % 多项式系数
     p1 = -5.447e-05;
     p2 = 0.003999;
@@ -33,38 +32,21 @@ function [d_aero_ang, Control_Propulsion] = Reference_Flight_Timeline(t, Ma)
     % 计算最佳升阻比对应攻角
     alpha_best = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
 
-    % 阶段时间点
-    t1 = 60; % 阶段 1 结束时间
-    t2 = 120; % 阶段 2 结束时间
-    t3 = 180; % 阶段 3 结束时间
+    % 过渡时间（秒）
+    transition_time = 5; % 增加过渡时间以平滑变化
 
-    % 方波参数
-    max_value = 5; % 方波最大值
-    min_value = 3; % 方波最小值
-    period = 60; % 方波周期
+    t1 = 60; % 发动机关机时间
 
-    if t <= t1
-        % 阶段 1：alpha = 6
+    if t < t1
         alpha = 6;
-    else
-        alpha = alpha_best;
-        % elseif t <= t2
-        %     % 阶段 2：最佳升阻比对应攻角
-        %     alpha = alpha_best;
-        % elseif t <= t3
-        %     % 阶段 3：方波
-        %     % 计算方波值
-        %     phase = mod(t - t2, period); % 当前时间点在周期内的位置
 
-        %     if phase < period / 2
-        %         alpha = max_value; % 方波高值
-        %     else
-        %         alpha = min_value; % 方波低值
-        %     end
+    elseif t < t1 + transition_time %过渡过程
+        alpha_start = 6;
+        alpha_end = alpha_best;
+        alpha = alpha_start + (alpha_end - alpha_start) * (t - t1) / transition_time;
+    else %最佳升阻比对应攻角
+        alpha = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
 
-        % else
-        %     % 阶段 4：alpha = alpha_best
-        %     alpha = alpha_best;
     end
 
     % 发动机推力控制规律
--- GHV_trajectory/reference_alpha.m
+++ GHV_trajectory/reference_alpha.m
@@ -2,7 +2,7 @@
 /*
  * @Author: blueWALL-E
  * @Date: 2025-10-23 22:15:21
- * @LastEditTime: 2025-12-10 11:33:22
+ * @LastEditTime: 2025-12-18 11:08:45
  * @FilePath: \GHV_open\GHV_trajectory\reference_alpha.m
  * @Description: 参考攻角变化规律
  * @Wearing:  Read only, do not modify place!!!
@@ -22,64 +22,21 @@ function alpha = reference_alpha(t, Ma)
     % 计算最佳升阻比对应攻角
     alpha_best = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
 
-    % 阶段时间点
-    t1 = 60; % 阶段 1 结束时间
-    t2 = 120; % 阶段 2 结束时间
-    t3 = 240; % 阶段 3 结束时间
+    % 过渡时间（秒）
+    transition_time = 5; % 增加过渡时间以平滑变化
 
-    % 方波参数
-    max_value = 5; % 方波最大值
-    min_value = 3; % 方波最小值
-    period = 20; % 方波周期
+    t1 = 60; % 发动机关机时间
 
-    if t <= t1
-        % 阶段 1：alpha = 6
+    if t < t1
         alpha = 6;
-    elseif t <= t2
-        % 阶段 2：最佳升阻比对应攻角
-        alpha = alpha_best;
-    elseif t <= t3
-        % 阶段 3：方波
-        % 计算方波值
-        phase = mod(t - t2, period); % 当前时间点在周期内的位置
 
-        if phase < period / 2
-            alpha = max_value; % 方波高值
-        else
-            alpha = min_value; % 方波低值
-        end
+    elseif t < t1 + transition_time %过渡过程
+        alpha_start = 6;
+        alpha_end = alpha_best;
+        alpha = alpha_start + (alpha_end - alpha_start) * (t - t1) / transition_time;
+    else %最佳升阻比对应攻角
+        alpha = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
 
-    else
-        % 阶段 4：alpha = alpha_best
-        alpha = alpha_best;
     end
 
 end
-
-% % 多项式系数
-% p1 = -5.447e-05;
-% p2 = 0.003999;
-% p3 = -0.1117;
-% p4 = 1.475;
-% p5 = -9.176;
-% p6 = 25.62;
-
-% % 计算最佳升阻比对应攻角
-% alpha_best = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
-
-% % 过渡时间（秒）
-% transition_time = 5; % 增加过渡时间以平滑变化
-
-% t1 = 60; % 发动机关机时间
-
-% if t < t1
-%     alpha = 6;
-
-% elseif t < t1 + transition_time %过渡过程
-%     alpha_start = 6;
-%     alpha_end = alpha_best;
-%     alpha = alpha_start + (alpha_end - alpha_start) * (t - t1) / transition_time;
-% else %最佳升阻比对应攻角
-%     alpha = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
-
-% end
