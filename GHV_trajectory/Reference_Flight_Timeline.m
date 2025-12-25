a399dcc05f44e2390c86bb301a26544135b46442
blueWALL-E <boyu.dragon@gmail.com>
2025-12-25 11:38:51 +0800

    feat(traj): 优化代码结构 飞行时序调用参考攻角（reference_alpha）函数
    分层调用，避免重复写代码

--- GHV_trajectory/Reference_Flight_Timeline.m
+++ GHV_trajectory/Reference_Flight_Timeline.m
@@ -2,7 +2,7 @@
 /*
  * @Author: blueWALL-E
  * @Date: 2025-10-19 14:52:06
- * @LastEditTime: 2025-12-18 11:09:11
+ * @LastEditTime: 2025-12-21 16:51:25
  * @FilePath: \GHV_open\GHV_trajectory\Reference_Flight_Timeline.m
  * @Description: 参考飞行时序
  * @Wearing:  Read only, do not modify place!!!
@@ -20,34 +20,36 @@
 %d_aero_ang: 单位 deg/s    期望气流角 3*1 （航迹滚转角 攻角 侧滑角）
 %Control_Propulsion:  单位 n.d. rad/s  发动机油门量与矢量控制角度 3*1
 function [d_aero_ang, Control_Propulsion] = Reference_Flight_Timeline(t, Ma)
-
-    % 多项式系数
-    p1 = -5.447e-05;
-    p2 = 0.003999;
-    p3 = -0.1117;
-    p4 = 1.475;
-    p5 = -9.176;
-    p6 = 25.62;
-
-    % 计算最佳升阻比对应攻角
-    alpha_best = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
-
-    % 过渡时间（秒）
+    % % 过渡时间（秒）
     transition_time = 5; % 增加过渡时间以平滑变化
 
     t1 = 60; % 发动机关机时间
 
-    if t < t1
-        alpha = 6;
+    % % 多项式系数
+    % p1 = -5.447e-05;
+    % p2 = 0.003999;
+    % p3 = -0.1117;
+    % p4 = 1.475;
+    % p5 = -9.176;
+    % p6 = 25.62;
 
-    elseif t < t1 + transition_time %过渡过程
-        alpha_start = 6;
-        alpha_end = alpha_best;
-        alpha = alpha_start + (alpha_end - alpha_start) * (t - t1) / transition_time;
-    else %最佳升阻比对应攻角
-        alpha = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
+    % % 计算最佳升阻比对应攻角
+    % alpha_best = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
 
-    end
+
+
+    % if t < t1
+    %     alpha = 6;
+
+    % elseif t < t1 + transition_time %过渡过程
+    %     alpha_start = 6;
+    %     alpha_end = alpha_best;
+    %     alpha = alpha_start + (alpha_end - alpha_start) * (t - t1) / transition_time;
+    % else %最佳升阻比对应攻角
+    %     alpha = p1 * Ma ^ 5 + p2 * Ma ^ 4 + p3 * Ma ^ 3 + p4 * Ma ^ 2 + p5 * Ma + p6;
+
+    % end
+    alpha = reference_alpha(t, Ma);
 
     % 发动机推力控制规律
     if t <= t1
