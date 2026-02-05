dc74f7ebd75f2669e01c64e42a5b97a07e373e98
blueWALL-E <boyu.dragon@gmail.com>
2026-02-05 21:22:36 +0800

    fix(dyn): 更新质心位置和转动惯量计算注释，修正输出变量名称

--- GHV_model/cg_inertia_variation.m
+++ GHV_model/cg_inertia_variation.m
@@ -2,7 +2,7 @@
 /*
  * @Author: blueWALL-E
  * @Date: 2025-09-22 10:10:22
- * @LastEditTime: 2025-09-22 23:59:27
+ * @LastEditTime: 2026-02-05 18:33:23
  * @FilePath: \GHV_open\GHV_model\cg_inertia_variation.m
  * @Description: 质心位置变化与转动惯量变化计算
  * @Wearing:  Read only, do not modify place!!!
@@ -12,12 +12,12 @@
 
 %cg_inertia_variation 质心位置变化与转动惯量变化计算
 %input
-% mass    单位 kg 飞行器当前质量
+% mass      单位 kg     飞行器当前质量
 
 %output
-% CG        单位 m 质心位置
+% x_cg      单位 m      质心位置
 % I         单位 kg*m^2 转动惯量矩阵 3*3
-% dI_dmass  单位 ? 转动惯量对质量的导数矩阵 3*3
+% dI_dmass  单位 m^2    转动惯量对质量的导数矩阵 3*3 数学表达式 dI_dmass = dI/dmass 单位本身无意义
 
 function [x_cg, I, dI_dmass] = cg_inertia_variation(mass)
     % 惯性矩
@@ -27,8 +27,7 @@ function [x_cg, I, dI_dmass] = cg_inertia_variation(mass)
     I = diag([Ixx, Iyy, Izz]);
 
     % 重心位置
-    x_cg_element = 1.65e-10 * mass ^ 2 -5.57e-5 * mass + 7.37;
-    x_cg = x_cg_element;
+    x_cg = 1.65e-10 * mass ^ 2 -5.57e-5 * mass + 7.37;
 
     % 转动惯量对质量的导数
     dIxx_dmass = 1991/100 - (5238875316933513 * mass) / 36893488147419103232;
