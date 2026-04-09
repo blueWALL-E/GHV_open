8792985c4514fdd8ae7e396803796f19478994de
blueWALL-E <boyu.dragon@gmail.com>
2026-04-10 00:51:34 +0800

    feat(GHV_control_param_optimization): 添加目标函数和约束条件的实现

---
+++ GHV_control_parameter_optimization/math/objfun_control_param.m
@@ -0,0 +1,41 @@
+%{
+/*
+ * @Author: blueWALL-E
+ * @Date: 2026-04-09 22:24:39
+ * @LastEditTime: 2026-04-09 23:03:59
+ * @FilePath: \GHV_open\GHV_control_parameter_optimization\math\objfun_control_param.m
+ * @Description: 目标函数构造
+ * @Wearing:  Read only, do not modify place!!!
+ * @Shortcut keys:  ctrl+alt+/ ctrl+alt+z
+ */
+ %}
+function J = objfun_control_param(control_param, model, blk_control_param)
+
+    % 1) 整个控制参数向量直接写入 Simulink
+
+    in = Simulink.SimulationInput(model);
+    in = in.setBlockParameter(blk_control_param, 'Value', mat2str(control_param));
+    in = in.setModelParameter('StopTime', '10');
+
+    % 2) 运行仿真
+    simOut = sim(in);
+
+    % 3) 读取输出信号
+    logs = simOut.logsout;
+    aero_ang = logs.get('aero_ang');
+    aero_ang_data = reshape_to_n3(aero_ang.Values.Data);
+    alpha_data = aero_ang_data(:, 2);
+    t = aero_ang.Values.Time;
+
+    % 4) 目标函数
+
+    S = stepinfo(alpha_data, t, 6);
+    J_IAE = trapz(t, abs(alpha_data - 6));
+    J_IAEref =1.439065738871150;
+    J_int = J_IAE / J_IAEref;
+    J_signal = S.Overshoot;
+    J_tss = S.SettlingTime;
+    J_tr = S.RiseTime;
+    J = 1 * J_int + 1 * J_signal + 1 * J_tss + 1 * J_tr;
+
+end
